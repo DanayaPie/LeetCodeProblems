e3d2fb144912866626b7eb741b8a97955562de14
DanayaPie <92035671+DanayaPie@users.noreply.github.com>
2022-07-20 15:52:10 -0400

    LengthOfLastWord

---
+++ src/codingProblems/LengthOfLastWord.java
@@ -0,0 +1,26 @@
+package codingProblems;
+
+public class LengthOfLastWord {
+
+    // only counting the length of the last word
+    public static void main(String[] args) {
+        String s = "  today is a nice day   ";
+
+        System.out.println(lengthOfLastWord(s));
+    }
+
+    private static int lengthOfLastWord(String s) {
+        int length = 0;
+        s = s.trim();
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) != ' ') {
+                length++;
+            } else {
+                break;
+            }
+        }
+
+        return length;
+    }
+}
