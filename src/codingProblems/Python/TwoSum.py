02105a983be0ff77c827ffecb3e08af505bb9ce0
DanayaPie <92035671+DanayaPie@users.noreply.github.com>
2022-08-01 13:40:24 -0400

    TwoSum

---
+++ src/codingProblems/Python/TwoSum.py
@@ -0,0 +1,29 @@
+nums = [0, 11, 9, 3]
+target = 9
+
+
+class Solution(object):
+
+    def __init__(self, nums, target):
+        self.nums = nums
+        self.target = target
+
+    def twoSum(self, nums, target):
+
+        arr = {}
+
+        for i, value in enumerate(nums):
+
+            diff = target - value
+
+            if diff in arr:
+                return [arr[diff], i]
+
+            arr[value] = i
+
+        return [-1, -1]
+
+
+res = Solution(nums, target)
+
+print(res.twoSum(nums, target))
