d6c1e933349eaad793888af33313e295e6f9490d
DanayaPie <92035671+DanayaPie@users.noreply.github.com>
2022-11-15 09:59:59 -0500

    ExcelSheetColumnNumber

---
+++ src/codingProblems/Java/ExcelSheetColumnNumber.java
@@ -0,0 +1,29 @@
+package codingProblems.Java;
+
+public class ExcelSheetColumnNumber {
+
+    /*-
+        Logic:
+            - Using ASCII -> 'A' = 65
+            - 26 characters in alphabet
+            - calculation: (i * 26) + characterValue
+     */
+    public static void main(String[] args) {
+        String columnTitle = "DANAYA";
+
+        System.out.println(titleToNumber(columnTitle));
+    }
+
+    private static Integer titleToNumber(String columnTitle) {
+
+        int res = 0;
+
+        for (int i = 0; i < columnTitle.length(); i++) {
+
+            res *= 26;
+            res += (columnTitle.charAt(i) - 'A' + 1); // '+ 1' to set 'A' to 1
+
+        }
+        return res;
+    }
+}
